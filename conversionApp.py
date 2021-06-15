591a872207cabe8d5b6f0f744018daa00266a5fa
M.ash <ashuza1411@gmail.com>
2021-06-15 08:19:48 +0200

    Add a main application

--- conversionApp.py
+++ conversionApp.py
@@ -9,42 +9,9 @@ from math import pi as p
     2. convert (user the formular) 
     3. Display the result 
 """
+# This is my functions 
 
-
-def main():
-    # Variable de passage 
-    continuer = True
-
-    print("Super Convertisseur Dgré-Radian")
-
-    # Boucle principale pour faire le chiox de converssion
-    while continuer:
-        # Affichage du menu 
-        choix = Menu()
-        # Traitement du choix de l'utilisateur comme une chaine des caracteur 
-        if choix == "1":
-            # Prendre et convertir l'angle 
-            deg = float(input("Mesure en degré: "))
-            # Affiche le resultat en le convertisant en chaine de caract̀ères
-            print("Mesure en radian : " + str(degre_en_rad(deg)))
-            # Demande si l'on veux continuer 
-            continuer = demande_continuer()
-        elif choix == "2":
-            # Idem dans l'autre sence de conversion 
-            rad = float(input("Mesure en radian : "))
-            print("Mesure en degré : " + str(radians_en_deg(rad)))
-            continuer = demande_continuer()
-        elif choix == "q":
-            # On veux quitter 
-            continuer = False 
-        else:
-            print("Option inconnue")
-
-print("\n Ce convertisseur vous est juteusement offert par Ash, cn")
-
-
-
-# Menu
+# Menu 
 def Menu():
     print("\nOptions disponibles :")
     print("1 : Convertir des degrés en radians")
@@ -70,8 +37,3 @@ def demande_continuer():
         return True
     else:
         return False
-
-
-
-if __name__ == "__main__":
-    main()
